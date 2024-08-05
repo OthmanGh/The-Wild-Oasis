0fc822a5a6fa02b25909b5eb25b2f5f7b1393dc2
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-06 01:07:44 +0300

    Use styled component props & css function

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const H1 = styled.h1`
-  font-size: 2rem;
-  background-color: lightblue;
-  color: darkblue;
-  padding: 1rem;
-`;
-
-const Button = styled.button`
-  background-color: black;
-  color: orange;
-  cursor: pointer;
-  padding: 1.2rem 1.5rem;
-  border-radius: 5px;
-  margin-right: 20px;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    color: black;
-    background-color: orange;
-  }
-`;
-
-const Input = styled.input`
-  border: none;
-  width: 250px;
-  padding: 1.2rem 2rem;
-  border-radius: 5px;
-  background-color: whitesmoke;
-
-  &:focus {
-    border: 1px solid gray;
-  }
-`;
+import Button from './ui/Button';
+import GlobalStyles from './styles/GlobalStyles';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 const StyledApp = styled.main`
-  background-color: green;
-  height: 100vh;
-  padding: 2rem;
+  background-color: orangered;
+  padding: 20px;
 `;
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('Check out')}>Check out</Button>
-      <Button onClick={() => alert('Check In')}>Check in</Button>
-      <Input type="number" placeholder="enter your age"></Input>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Heading as="h2">Test H2</Heading>
+
+        <Button onClick={() => alert('Check In')}>Check in</Button>
+        <Input type="number" placeholder="enter your age"></Input>
+
+        <Heading as="h3"> Test H3</Heading>
+      </StyledApp>
+    </>
   );
 };
 
